a2d9a951f99a1cb7ce5b0643c648a71f2c95671c
zeid <zeidhassan4@gmail.com>
2023-12-13 00:26:47 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+ if (number <= 400){
+    return 'This one is on me!'
+ }
+  else if (number > 2000 && number <=2500 ){
+  return 'I will gladly take your thirty bucks.'
+ }
+ else {(number > 2500 )  
+   return 'No can do.'
+ }
+ }
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  }
+  else {(city ==='NYC')
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === 'generous'){
+    return  'Thank you so much.'
+  }
+  else if (tip === 'not as generous'){
+    return  'Thank you.'
+  } 
+  else{ (tip === 'thanks for everything') 
+  return 'Bye.'
+  }
 }
